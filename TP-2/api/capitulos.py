42c6e326cfeb37bcbf2362e97e47af63e6cb9682
GonzaloE <errandoneagonzalo18@gmail.com>
2023-04-09 02:36:58 -0300

    tp2 por la mitad

---
+++ TP-2/api/capitulos.py
@@ -0,0 +1,8 @@
+capitulos = [{'name': 'The Mandalorian', 'status': 'disponible'},
+              {'name': 'The Child', 'status': 'disponible'},
+              {'name': 'The Sin', 'status': 'disponible'},
+              {'name': 'Sanctuary', 'status': 'disponible'},
+              {'name': 'The Gunslinger', 'status': 'disponible'},
+              {'name': 'The Prisoner', 'status': 'disponible'},
+              {'name': 'The Reckoning', 'status': 'disponible'},
+              {'name': 'Redemption', 'status': 'disponible'}]
